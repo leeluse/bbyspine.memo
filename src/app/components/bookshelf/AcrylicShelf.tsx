4ba2ff40a4484e40c59a36f35fb7a8ee03dea9f4
Bizpeer1 <lgh0385hh@gmail.com>
2026-09-02 13:24:17 +0900

    fix: BookShelf 아크릴 반응형 추가

--- src/app/components/bookshelf/AcrylicShelf.tsx
+++ src/app/components/bookshelf/AcrylicShelf.tsx
@@ -1,6 +1,6 @@
 export default function AcrylicShelf() {
     return (
-        <div className="relative shrink-0 w-11/12 sm:w-11/12 lg:w-8/12 h-5 rounded-md border border-white/30 border-b-white/10 bg-linear-to-b from-white/20 via-sky-100/5 to-white/5 backdrop-blur-md shadow-[0_10px_25px_rgba(15,23,42,0.15),inset_0_1px_1px_rgba(255,255,255,0.4)]">
+        <div className="relative shrink-0 w-11/12 sm:w-11/12 lg:w-8/12 h-3 sm:h-5 rounded-sm sm:rounded-md border border-white/30 border-b-white/10 bg-linear-to-b from-white/20 via-sky-100/5 to-white/5 backdrop-blur-md shadow-[0_10px_25px_rgba(15,23,42,0.15),inset_0_1px_1px_rgba(255,255,255,0.4)]">
             <div className="absolute top-0.75 left-1 right-1 h-px bg-linear-to-r from-transparent via-white/80 to-transparent" />
             <div className="absolute top-0 bottom-0 left-0 w-px bg-linear-to-b from-white/50 to-transparent" />
             <div className="absolute top-0 bottom-0 right-0 w-px bg-linear-to-b from-white/50 to-transparent" />
